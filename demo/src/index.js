85e941f359165038bbbecbb35f302ba55c9cae64
rainkolwa <pluvo.lee@gmail.com>
2017-09-06 15:25:10 +0800

    init

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,62 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-
+import { css } from 'glamor'
 import Example from '../../src'
 
+const imgs = [
+  'https://olcqde7ua.qnssl.com/Fl96hDtarkXwJWs2FoKkfyTJ9JdJ?imageView2/1/w/480/h/480',
+  'https://olcqde7ua.qnssl.com/FlGCkPbOmqGqmf6YaJDjivOAw7P8?imageView2/1/w/480/h/480',
+]
+
+const style = css({
+  margin: 0,
+  padding: 0,
+  overflow: 'hidden',
+  ' li': { float: 'left', listStyleType: 'none', margin: '10px' },
+  ' li img': { width: '100px' }
+})
+
 class Demo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false,
+      activeIndex: 0,
+    }
+  }
+  
+  handleView = (index) => {
+    this.setState({
+      active: true,
+      activeIndex: index,
+    })
+  }
+  
+  hideBox = () => {
+    this.setState({
+      active: false,
+    })
+  }
+  
   render() {
+    const { active, activeIndex } = this.state
     return <div>
       <h1>fancy-weui-box Demo</h1>
-      <Example/>
+      <ul {...style}>
+        {imgs.map((img, index)=> {
+          return (
+            <li key={index} onClick={()=>this.handleView(index)}>
+              <img src={img} alt=""/>
+            </li>    
+          )
+        })}
+      </ul>
+      <Example 
+        active={active}
+        activeIndex={activeIndex}
+        imgs={imgs}
+        hide={this.hideBox}
+      />
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 import React, {Component} from 'react'
+import { css } from 'glamor'
+import SwipeableViews from 'react-swipeable-views'
 
-export default class extends Component {
+const style = css({
+  position: 'fixed',
+  left: '100%',
+  top: 0,
+  bottom: 0,
+  width: '100%',
+  height: '100%',
+  background: 'black',
+  transition: 'all 0.4s'
+})
+
+const imgBox = css({
+  position: 'relative',
+  left: 0,
+  top: 0,
+  bottom: 0,
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  ' img': { width: '100%' }
+})
+
+class FancyWeuiBox  extends Component {
+  
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    const { active, activeIndex, imgs, hide } = this.props
+    console.log(activeIndex)
+    return (
+      <div {...css(style, { left: active ? 0 : '100%' })}>
+        <SwipeableViews index={activeIndex}>
+          {imgs.map((img, index) => {
+            return (
+              <div key={index} {...imgBox}>
+                <img src={img} onClick={hide} alt=""/>
+              </div>
+            )
+          })}
+        </SwipeableViews>
+      </div>
+    )
   }
 }
+
+export default FancyWeuiBox
