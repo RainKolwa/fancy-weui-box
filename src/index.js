63c6297da86b347c6978e9aa051cbf4915ae14d2
rainkolwa <pluvo.lee@gmail.com>
2017-09-08 14:34:40 +0800

    style

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import { css } from 'glamor'
 import SwipeableViews from 'react-swipeable-views'
 
@@ -10,40 +11,61 @@ const style = css({
   width: '100%',
   height: '100%',
   background: 'black',
-  transition: 'all 0.4s'
+  transition: 'all 0.4s',
+  ' .fancy-weui-container': {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%',
+    ' .react-swipeable-view-container': {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      bottom: 0,
+      width: '100%',
+      height: '100%',
+      ' >div': {
+        top: 0,
+        bottom: 0,
+        display: 'flex',
+        alignItems: 'center',
+        background: 'black',
+        ' img': { width: '100%' }
+      },
+    },
+  },
 })
 
-const imgBox = css({
-  position: 'relative',
-  left: 0,
-  top: 0,
-  bottom: 0,
-  width: '100%',
-  height: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  ' img': { width: '100%' }
-})
+class FancyWeuiBox extends Component {
 
-class FancyWeuiBox  extends Component {
-  
   render() {
     const { active, activeIndex, imgs, hide } = this.props
-    console.log(activeIndex)
     return (
       <div {...css(style, { left: active ? 0 : '100%' })}>
-        <SwipeableViews index={activeIndex}>
+        <SwipeableViews index={activeIndex} className="fancy-weui-container">
           {imgs.map((img, index) => {
             return (
-              <div key={index} {...imgBox}>
-                <img src={img} onClick={hide} alt=""/>
-              </div>
+              <img key={index} src={img} onClick={hide} alt=""/>
             )
           })}
         </SwipeableViews>
       </div>
     )
   }
+}
+
+FancyWeuiBox.PropTypes = {
+  active: PropTypes.bool,
+  activeIndex: PropTypes.number,
+  imgs: PropTypes.array,
+  hide: PropTypes.func,
+}
+
+FancyWeuiBox.defaultProps = {
+  active: false,
+  activeIndex: 0,
 }
 
 export default FancyWeuiBox
